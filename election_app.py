973701515aacb6b6f0c1edb9d75cd3baeebde6d8
Giri-445 <giridharanpnkg@gmail.com>
2024-04-06 12:28:16 +0530

    Update election_app.py

--- election_app.py
+++ election_app.py
@@ -3,7 +3,6 @@ import sqlite3 as sq
 import random as rd
 import string
 from datetime import  date, datetime, timedelta
-import id_card_generator as id
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
@@ -232,7 +231,7 @@ create_database()
 
 def add_voter(name, constituency, booth_number, dob, gender, photo):
     voter_id = generate_voter_id()
-    voter_id_card_img = id.generate_id_card(name, constituency, booth_number, dob, gender,voter_id, photo)
+    voter_id_card_img = generate_id_card(name, constituency, booth_number, dob, gender,voter_id, photo)
 
  
     photo_data = photo.read()
@@ -416,6 +415,41 @@ def view_election_result():
         st.success(f"The party with the most constituency wins: {most_wins_party} ({party_wins[most_wins_party]} constituencies)")
     else:
         st.warning("No election results found.")
+
+
+
+def generate_id_card(name, constituency, booth_number, dob, gender,voter_id, photo):
+    photo = Image.open(photo)
+    id_card = Image.open("voter_idcard_template.jpg")
+
+    # Resize the photo to fit a specific region on the ID card
+    photo_width, photo_height = 200, 250  # Define the size of the region for pasting the photo
+    photo = photo.resize((photo_width, photo_height))
+
+    # Paste the resized photo onto the ID card template
+    id_card.paste(photo, (140, 240))
+
+    # Initialize drawing context
+    draw = ImageDraw.Draw(id_card)
+
+    # Load a font
+    font_path = "arial.ttf" 
+    try:
+        font = ImageFont.truetype(font_path, 20)
+    except OSError:
+    # Fallback to a default font if the specified font is not available
+        font = ImageFont.load_default()
+
+    draw.text((320,170), voter_id, fill="black", font=font)
+
+    # Write details on the image
+    draw.text((80, 520), f"Name: {name}", fill="black", font=font)
+    draw.text((80, 560), f"D.O.B: {dob}", fill="black", font=font)
+    draw.text((80, 600), f"Gender: {gender}", fill="black", font=font)
+    draw.text((80, 640), f"Voter_Id: {voter_id}", fill="black", font=font)
+
+
+    return id_card
 
 
 
